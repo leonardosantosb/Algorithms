d93392392be1828b3d042943453017df09f15d40
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-07-06 18:37:08 -0300

    req 2 - testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,27 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("testando", 3) == "set_odnat"
+
+    assert encrypt_message("testando", 5) == "atset_odn"
+
+    assert encrypt_message("testando", 4) == "odna_tset"
+
+    assert encrypt_message("testando", 6) == "od_natset"
+
+    assert encrypt_message("testando", 0) == "odnatset"
+
+    assert encrypt_message("testando", -2) == "odnatset"
+
+    assert encrypt_message("testando", 10) == "odnatset"
+
+    assert encrypt_message("", 3) == ""
+
+    with pytest.raises(TypeError):
+        encrypt_message(3, "testando")
+
+    with pytest.raises(TypeError):
+        encrypt_message("testando", "xuxa")
