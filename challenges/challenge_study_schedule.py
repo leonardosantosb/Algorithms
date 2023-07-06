9c08b05f383263b0f9d82c9951493342ed8f326d
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-07-06 15:38:30 -0300

    req 1 - refatorado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
+    if not target_time:
         return None
 
     count = 0
     for e, s in permanence_period:
-        if not (isinstance(target_time, int) and isinstance(e, int) and isinstance(s, int)):
+        if not (isinstance(target_time, int) and
+                isinstance(e, int) and isinstance(s, int)):
             return None
         if e <= target_time <= s:
             count += 1
@@ -12,6 +13,4 @@ def study_schedule(permanence_period, target_time):
     return count
 
 
-
-
-    raise NotImplementedError
+raise NotImplementedError
